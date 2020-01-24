f36aff491d0e899c2d55a1f7071d191b9c7bcb67
Baga Gusdiana <baga.gusdiana@len.co.id>
2020-01-24 08:25:10 +0700

    change last night

--- src/test/java/baga/len/unittesting/ListAllTest.java
+++ src/test/java/baga/len/unittesting/ListAllTest.java
@@ -2,9 +2,10 @@ package baga.len.unittesting;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.mockito.Mockito.*;
-
+import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,5 +22,28 @@ public class ListAllTest {
         verify(mock, atLeastOnce()).get(anyInt());
         verify(mock, atMost(2)).get(anyInt());
         verify(mock, never()).get(2);
+    }
+
+    @Test
+    public void argumentCapturing() {
+        mock.add("DataCapture");
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(mock).add(captor.capture());
+        assertEquals("DataCapture", captor.getValue());
+    }
+
+    @Test
+    public void multipleArgumentCapturing() {
+        mock.add("Data1");
+        mock.add("Data2");
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(mock, times(2)).add(captor.capture());
+
+        List<String> listCaptor = captor.getAllValues();
+
+        assertEquals("Data1", listCaptor.get(0));
+        assertEquals("Data2", listCaptor.get(1));
     }
 }
